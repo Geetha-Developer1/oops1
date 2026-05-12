448909057b78ad0051e3030b9448647eb028d370
DESKTOP-UA0O1ET\geeth <geethaprasanna422@gmail.com>
2026-05-12 12:45:57 +0530

    c

---
+++ oops1/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace oops1
+{
+    internal class Student
+    {
+        public int id;
+        public string name;
+        public int age;
+        public static string clgname;
+
+        public void details(int num)
+        {
+            Console.WriteLine("Enter College name: ");
+            clgname = Console.ReadLine();
+            for (int i=0;i<num;i++)
+            {
+                Console.WriteLine("Enter Student ID: ");
+                id = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Student Name: ");
+                name = Console.ReadLine();
+                Console.WriteLine("Enter Student Age: ");
+                age= int.Parse(Console.ReadLine());
+                Console.WriteLine("------------------------------");
+                Console.WriteLine("Student ID  : "+id);
+                Console.WriteLine("Student Name: "+name);
+                Console.WriteLine("Student age : "+age);
+                Console.WriteLine("College Name:" + clgname+"\n\n");
+            }
+           
+        }
+        static void Main(string[] args)
+        {
+            Student std = new Student();
+            Console.WriteLine("Enter Number of students you want:");
+            int num=int.Parse(Console.ReadLine());
+            std.details(num);
+
+          
+        }
+    }
+}
--- oops1/withrt_withparams.cs
+++ oops1/withrt_withparams.cs
@@ -21,15 +21,15 @@ namespace oops1
     }
     internal class withrt_withparams
     {
-        static void Main(string[] args)
-        {
-            class1 obj1 = new class1();
-            int res = obj1.sum1(11, 22);
-            Console.WriteLine("The return sum result: " + res);
-            string resname = obj1.methname("Rama");
-            Console.WriteLine("The return Name result: " + resname);
+        //static void Main(string[] args)
+        //{
+        //    class1 obj1 = new class1();
+        //    int res = obj1.sum1(11, 22);
+        //    Console.WriteLine("The return sum result: " + res);
+        //    string resname = obj1.methname("Rama");
+        //    Console.WriteLine("The return Name result: " + resname);
 
-        }
+        //}
 
     }
 }
